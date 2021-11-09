22701c241cfbec6bb9c8c088fbf90f2494c3ee17
opilane <jaan.krohin@gmail.com>
2021-11-09 10:17:07 +0200

    Ülessanne 403 on tehtud

---
+++ harjutused/403.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Ülesanne 403</title>
+</head>
+<body style="background-color: grey">
+<h1>Ülesanne 403</h1>
+<?php
+//värvi nimed
+$colours=array("AliceBlue","CadetBlue","Crimson","DarkGoldenRod","DarkMagenta","FireBrick","GhostWhite","LightGreen","LightYellow","MediumSpringGreen","MediumTurquoise","MidnightBlue","Olive","Orange","Teal");
+for ($i=0; $i <15;$i++){
+    echo "<span style='color: $colours[$i]'>$colours[$i]< /span ><br>";
+}
+?>
+</body>
+</html>
